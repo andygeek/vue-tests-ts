d2f7bf103d5e55bfe6a587102ab3102c9a562d91
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-19 00:29:45 -0500

    Creando la prueba para un emit

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="name"/>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class LoginForm extends Vue {
+  name: string = ''
+
+  onSubmit() : void {
+    this.$emit('formSubmitted', { name: this.name })
+  }
+}
+</script>
