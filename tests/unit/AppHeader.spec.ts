a50d1eede2c6bf0d092520b725ced3bda75c5114
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-18 22:48:18 -0500

    Prueba que falla por no ser asincrona

--- tests/unit/AppHeader.spec.ts
+++ tests/unit/AppHeader.spec.ts
@@ -8,4 +8,12 @@ describe('AppHeader', () => {
     const wrapper = mount(AppHeader)
     expect(wrapper.find('button').isVisible()).toBe(false)
   })
+
+  test('If user is no logged in, do not show logout button', () => {
+    const wrapper = mount(AppHeader)
+    // Esto falla porque con mount ya renderizamos el DOM, y luego realizamos el cambio
+    // Pero estas llamadas no son asincronas, por lo que hay un tiempo hasta que el boton sea visible
+    wrapper.setData({ loggedIn: true })
+    expect(wrapper.find('button').isVisible()).toBe(true)
+  })
 })
